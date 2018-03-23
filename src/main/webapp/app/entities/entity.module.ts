2a03968df4dd477857b7e7edf98ee659c11dc213
GYL <gyl@civadis.be>
2018-03-23 09:53:58 +0100

    domain objects from jdl have been added

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,25 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { PlamobDomaineModule } from './domaine/domaine.module';
+import { PlamobMissionModule } from './mission/mission.module';
+import { PlamobLivrableModule } from './livrable/livrable.module';
+import { PlamobJournalSuiviModule } from './journal-suivi/journal-suivi.module';
+import { PlamobAffectationModule } from './affectation/affectation.module';
+import { PlamobRessourceModule } from './ressource/ressource.module';
+import { PlamobProfilModule } from './profil/profil.module';
+import { PlamobResponsabiliteAffectationModule } from './responsabilite-affectation/responsabilite-affectation.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        PlamobDomaineModule,
+        PlamobMissionModule,
+        PlamobLivrableModule,
+        PlamobJournalSuiviModule,
+        PlamobAffectationModule,
+        PlamobRessourceModule,
+        PlamobProfilModule,
+        PlamobResponsabiliteAffectationModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
