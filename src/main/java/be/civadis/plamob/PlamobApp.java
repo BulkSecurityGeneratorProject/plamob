ed6db41bc969bb5a19c0e30276643ffa1724db3f
GYL <gyl@civadis.be>
2018-03-23 09:47:56 +0100

    jhipster project created and push

--- src/main/java/be/civadis/plamob/PlamobApp.java
+++ src/main/java/be/civadis/plamob/PlamobApp.java
@@ -46,7 +46,7 @@ public class PlamobApp {
         Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
         if (activeProfiles.contains(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT) && activeProfiles.contains(JHipsterConstants.SPRING_PROFILE_PRODUCTION)) {
             log.error("You have misconfigured your application! It should not run " +
-                "with both the 'dev' and 'prod' profiles at the same time. test develop");
+                "with both the 'dev' and 'prod' profiles at the same time.");
         }
         if (activeProfiles.contains(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT) && activeProfiles.contains(JHipsterConstants.SPRING_PROFILE_CLOUD)) {
             log.error("You have misconfigured your application! It should not " +
