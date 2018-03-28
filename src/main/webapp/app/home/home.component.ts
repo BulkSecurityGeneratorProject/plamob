94bababab125d0d5c04c3dae95ad90e47ca36502
GYL <gyl@civadis.be>
2018-03-28 11:20:14 +0200

    Some parts of the UI updated (suite)

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
 
 import { Account, LoginModalService, Principal } from '../shared';
+import { LoginService } from '../shared/login/login.service';
 
 @Component({
     selector: 'jhi-home',
@@ -16,10 +17,17 @@ export class HomeComponent implements OnInit {
     account: Account;
     modalRef: NgbModalRef;
 
+    /********/
+    username: string;
+    password:string;
+    rememberMe:boolean;
+    /********/
+
     constructor(
         private principal: Principal,
         private loginModalService: LoginModalService,
-        private eventManager: JhiEventManager
+        private eventManager: JhiEventManager,
+        private loginService: LoginService
     ) {
     }
 
@@ -43,6 +51,38 @@ export class HomeComponent implements OnInit {
     }
 
     login() {
-        this.modalRef = this.loginModalService.open();
+        this.loginService.login({
+            username: this.username,
+            password: this.password,
+            rememberMe: this.rememberMe
+        }); /*.then(() => {
+            this.authenticationError = false;
+            this.activeModal.dismiss('login success');
+            if (this.router.url === '/register' || (/^\/activate\//.test(this.router.url)) ||
+                (/^\/reset\//.test(this.router.url))) {
+                this.router.navigate(['']);
+            }
+
+            this.eventManager.broadcast({
+                name: 'authenticationSuccess',
+                content: 'Sending Authentication Success'
+            });
+
+            // // previousState was set in the authExpiredInterceptor before being redirected to login modal.
+            // // since login is succesful, go to stored previousState and clear previousState
+            const redirect = this.stateStorageService.getUrl();
+            if (redirect) {
+                this.stateStorageService.storeUrl(null);
+                this.router.navigate([redirect]);
+            }
+        }).catch(() => {
+            this.authenticationError = true;
+        });*/
     }
+
+
+
+    /*login() {
+        this.modalRef = this.loginModalService.open();
+    }*/
 }
